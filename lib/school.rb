1389fdc8cc7f5e5975a5d18c1c8dd28d04936076
Tristan Gibbs <gtristan89@gmail.com>
2021-01-31 19:33:05 -0700

    commit tree 0c517766fff5ffb8c614a08c5f0c75a38f8fe6f7

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, year)
+        roster[year] ||= []
+        roster[year] << student
+    end
+
+    def grade(year)
+        roster.detect do |t, g|
+            if t == year
+                return g
+            end
+        end
+    end
+
+    def sort
+        new_hash = {}
+        roster.each do |t, g|
+            new_hash[t] = g.sort
+        end
+        new_hash
+    end
+
+    
+end
